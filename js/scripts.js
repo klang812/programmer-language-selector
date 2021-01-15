99ff75fd4e3eb9ca906b80cf32c3c66d4818a2d4
Kerry Lang <klang812@gmail.com>
2021-01-15 11:17:27 -0800

    add else if with working || &&

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,9 @@ $(document).ready(function() {
     const question4 = $("select#question4").val();
     const question5 = $("select#question5").val();
 
-    if (question1 === "Flexibility" || "Complexity" )  {
+    if (question1 === ("Flexibility" || "Dynamic") && question2 === "Hulu" && question3 === "Thailand" && question4 === "Quinoa" && question5 === "Fishing")  {
       $('#ruby').show();
-    // } else if (question2 === "Hulu"   "Beets" && "Fishing") {
+    } else if (question2 === "Hulu" && question1 === ("Flexibility" || "Dynamic" || "Complexity") && question3 === ("Thailand" || "Italy" || "Costa Rica") && (question4 === "Quinoa" || "Roasted Bell Peppers" || "Beets") && (question5 === "Fishing" || "Hiking" || "Smoking Meat")) {
     //   $('#ruby').show();
     // } else if (question2 === "Dynamic" || "Complexity" && "Google" || "Microsoft" && "Italy" || "Costa Rica" && "Roasted Bell Peppers" || "Beets" && "Hiking" || "Fishing") {
     //   $('#c#').show();
